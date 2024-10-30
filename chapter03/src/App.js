0169642e5988be134c33ad86fe0f5e97ce156916
VhukhudoCudo <vhukhudocudo@gmail.com>
2024-10-30 19:15:52 +0200

    commit

---
+++ chapter03/src/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import Products from './Products';
+import Rating from './Rating'; 
+import { Button } from 'react-bootstrap';
+
+ function App() {
+//const is valid
+
+//const isValid = true;
+
+return ( 
+  <div> 
+  <Rating rating='1' /> 
+  <Rating rating='2' /> 
+  <Rating rating='3' /> 
+  <Rating rating='4' /> 
+  <Rating rating='5' /> 
+  </div>
+  );
+}
+
+export default App;
---
+++ chapter03/src/Products.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function Products() {
+    const products = ["Learning React","Pro React","Beginning React"]; 
+const listProducts = products.map((product) =>  
+<li key={product.toString()}>{product}</li> 
+);
+    return (
+        <div>
+            {/* <h1>  Products</h1> */}         
+<ul>{listProducts}</ul>  
+        </div>
+    );
+}
+
+export default Products;  
